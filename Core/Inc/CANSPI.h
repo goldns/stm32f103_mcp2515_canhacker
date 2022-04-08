d15b5487f0ad4e782229f82cf0fbbde2db137a03
goldns <kam@goldns.ru>
2022-04-08 15:24:44 +0300

    First commit
    work : connect and recive ;)

---
+++ Core/Inc/CANSPI.h
@@ -0,0 +1,53 @@
+#ifndef __CAN_SPI_H
+#define	__CAN_SPI_H
+
+#include "stm32f1xx_hal.h"
+#include <stdbool.h>
+
+
+typedef union {
+  struct {
+    uint8_t idType;
+    uint32_t id;
+    uint8_t dlc;
+    uint8_t data[8];
+  } frame;
+  uint8_t array[14];
+} uCAN_MSG;
+
+
+#define dSTANDARD_CAN_MSG_ID_2_0B 1
+#define dEXTENDED_CAN_MSG_ID_2_0B 2
+
+bool CANSPI_Initialize(void);
+void CANSPI_Sleep(void);
+uint8_t CANSPI_Transmit(uCAN_MSG *tempCanMsg);
+uint8_t CANSPI_Receive(uCAN_MSG *tempCanMsg);
+uint8_t CANSPI_messagesInBuffer(void);
+uint8_t CANSPI_isBussOff(void);
+uint8_t CANSPI_isRxErrorPassive(void);
+uint8_t CANSPI_isTxErrorPassive(void);
+
+
+
+#define hex_asc_upper_lo(x)    hex_asc_upper[((x) & 0x0F)]
+#define hex_asc_upper_hi(x)    hex_asc_upper[((x) & 0xF0) >> 4]
+#define put_sff_id(buf, id) _put_id(buf, 2, id)
+#define put_eff_id(buf, id) _put_id(buf, 7, id)
+
+/* special address description flags for the CAN_ID */
+#define CAN_EFF_FLAG 0x80000000UL /* EFF/SFF is set in the MSB */
+#define CAN_RTR_FLAG 0x40000000UL /* remote transmission request */
+#define CAN_ERR_FLAG 0x20000000UL /* error message frame */
+
+/* valid bits in CAN ID for frame formats */
+#define CAN_SFF_MASK 0x000007FFUL /* standard frame format (SFF) */
+#define CAN_EFF_MASK 0x1FFFFFFFUL /* extended frame format (EFF) */
+#define CAN_ERR_MASK 0x1FFFFFFFUL /* omit EFF, RTR, ERR flags */
+
+
+
+
+
+
+#endif	/* __CAN_SPI_H */
